fd1bbd691accd0ecf04ce3b5bcd30826ee402b23
DanielSnd <daniel.snd.anim@gmail.com>
2024-02-23 12:35:03 -0400

    Added a dictionary/json based save system and method to compare time elapsed
    Added a dictionary/json based save system and method to compare time elapsed

--- yarnengine.cpp
+++ yarnengine.cpp
@@ -1,5 +1,8 @@
 #include "yarnengine.h"
 
+#include "core/io/dir_access.h"
+#include "core/io/json.h"
+
 YarnEngine* YarnEngine::singleton = nullptr;
 
 YarnEngine * YarnEngine::get_singleton() {
@@ -23,8 +26,40 @@ void YarnEngine::_bind_methods() {
     ClassDB::bind_method(D_METHOD("get_pause_independent_time"), &YarnEngine::get_pause_independent_time);
     ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "pause_independent_time"), "set_pause_independent_time", "get_pause_independent_time");
 
+    ClassDB::bind_method(D_METHOD("has_time_elapsed", "time_checking", "interval"), &YarnEngine::has_time_elapsed);
+    ClassDB::bind_method(D_METHOD("has_pause_independent_time_elapsed", "time_checking", "interval"), &YarnEngine::has_pause_independent_time_elapsed);
+
     ClassDB::bind_method(D_METHOD("test_callback_thing", "callable"), &YarnEngine::test_callback_thing);
+
+    ClassDB::bind_method(D_METHOD("set_save_path", "save_path"), &YarnEngine::set_save_path);
+    ClassDB::bind_method(D_METHOD("get_save_path"), &YarnEngine::get_save_path);
+    ADD_PROPERTY(PropertyInfo(Variant::STRING, "save_path"), "set_save_path", "get_save_path");
+    
+    ClassDB::bind_method(D_METHOD("set_save_backup_path", "save_backup_path"), &YarnEngine::set_save_backup_path);
+    ClassDB::bind_method(D_METHOD("get_save_backup_path"), &YarnEngine::get_save_backup_path);
+    ADD_PROPERTY(PropertyInfo(Variant::STRING, "save_backup_path"), "set_save_backup_path", "get_save_backup_path");
+    
+    ClassDB::bind_method(D_METHOD("set_save_requested", "save_requested"), &YarnEngine::set_save_requested);
+    ClassDB::bind_method(D_METHOD("get_save_requested"), &YarnEngine::get_save_requested);
+    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "save_requested"), "set_save_requested", "get_save_requested");
+    
+    ClassDB::bind_method(D_METHOD("set_save_data", "save_data"), &YarnEngine::set_save_data);
+    ClassDB::bind_method(D_METHOD("get_save_data"), &YarnEngine::get_save_data);
+    ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "save_data"), "set_save_data", "get_save_data");
+
+    ClassDB::bind_method(D_METHOD("add_to_save_data", "save_key","adding"), &YarnEngine::add_to_save_data);
+
+    ClassDB::bind_method(D_METHOD("set_to_save", "save_key","save"), &YarnEngine::set_to_save);
+
+    ClassDB::bind_method(D_METHOD("get_from_save", "save_key","default"), &YarnEngine::get_from_save);
+
+    ClassDB::bind_method(D_METHOD("request_save", "immediate"), &YarnEngine::request_save,DEFVAL(false));
+
     ADD_SIGNAL(MethodInfo("changed_pause", PropertyInfo(Variant::BOOL, "pause_value")));
+
+    ADD_SIGNAL(MethodInfo("after_full_save_reset"));
+    ADD_SIGNAL(MethodInfo("prepare_save"));
+    ADD_SIGNAL(MethodInfo("loaded_save", PropertyInfo(Variant::DICTIONARY, "save_data")));
 }
 
 Node* YarnEngine::get_current_scene() {
@@ -36,6 +71,104 @@ void YarnEngine::setup_node() {
         SceneTree::get_singleton()->get_root()->add_child(this);
         already_setup_in_tree=true;
     }
+}
+
+void YarnEngine::create_save_backup() {
+    auto dir = DirAccess::create(DirAccess::ACCESS_USERDATA);
+    if (!dir->file_exists(save_path)) {
+        return;
+    }
+    auto file = FileAccess::open(save_path, FileAccess::READ);
+    auto file_string = file->get_as_text();
+    file->close();
+    if (!file_string.is_empty()) {
+        auto write = FileAccess::open(save_backup_path, FileAccess::WRITE);
+        write->store_string(file_string);
+        write->close();
+    }
+}
+
+Variant YarnEngine::get_from_save(String p_save_key, Variant p_save_default) {
+    if (p_save_key.contains("/")) {
+        auto splitted = p_save_key.split("/");
+        Dictionary* desired_dict = &save_data;
+        for (int i = 0; i < splitted.size(); ++i) {
+            if (i == splitted.size()-1)
+                return desired_dict->get(p_save_key,p_save_default);
+            if (!desired_dict->has(splitted[i])) return p_save_default;
+            auto found_pointer = Object::cast_to<Dictionary>(desired_dict->get(splitted[i],Dictionary{}));
+            if (found_pointer != nullptr) {
+                desired_dict = found_pointer;
+            } else {
+                return p_save_default;
+            }
+        }
+    }
+    return save_data.get(p_save_key,p_save_default);
+}
+
+YarnEngine *YarnEngine::set_to_save(String p_save_key, Variant p_save_value) {
+    if (p_save_key.contains("/")) {
+        auto splitted = p_save_key.split("/");
+        Dictionary* desired_dict = &save_data;
+        for (int i = 0; i < splitted.size(); ++i) {
+            if (i == splitted.size()-1) {
+                if (desired_dict->has(splitted[i])) {
+                    desired_dict->erase(splitted[i]);
+                }
+                desired_dict->get_or_add(splitted[i],p_save_value);
+                return this;
+            }
+            if (!desired_dict->has(splitted[i]))
+                desired_dict->get_or_add(splitted[i],Dictionary{});
+            auto found_pointer = Object::cast_to<Dictionary>(desired_dict->get(splitted[i],Dictionary{}));
+            if (found_pointer != nullptr) {
+                desired_dict = found_pointer;
+            } else {
+                ERR_PRINT(vformat("FAILED TO ADD DICTIONARY WITH STRING %s original %s",splitted,p_save_key));
+            }
+        }
+    }
+    save_data[p_save_key] = p_save_value;
+    return this;
+}
+
+
+
+YarnEngine *YarnEngine::add_to_save_data(String p_save_key, Variant p_save_value) {
+    if (p_save_key.contains("/")) {
+        auto splitted = p_save_key.split("/");
+        Dictionary* desired_dict = &save_data;
+        for (int i = 0; i < splitted.size(); ++i) {
+            if (i == splitted.size()-1) {
+                float initial_value = desired_dict->get(splitted[i],0.0);
+                if (desired_dict->has(splitted[i])) {
+                    desired_dict->erase(splitted[i]);
+                }
+                desired_dict->get_or_add(splitted[i],static_cast<float>(p_save_value) + initial_value);
+                return this;
+            }
+            if (!desired_dict->has(splitted[i]))
+                desired_dict->get_or_add(splitted[i],Dictionary{});
+            auto found_pointer = Object::cast_to<Dictionary>(desired_dict->get(splitted[i],Dictionary{}));
+            if (found_pointer != nullptr) {
+                desired_dict = found_pointer;
+            } else {
+                ERR_PRINT(vformat("FAILED TO ADD DICTIONARY WITH STRING %s original %s",splitted,p_save_key));
+            }
+        }
+    }
+    else {
+        if (save_data.has(p_save_key)) {
+            auto current = save_data[p_save_key];
+            if (current.is_num() && p_save_value.is_num()) {
+                save_data[p_save_key] = Variant{static_cast<float>(current) + static_cast<float>(p_save_value)};
+            }
+        } else {
+            save_data[p_save_key] = p_save_value;
+        }
+    }
+    return this;
 }
 
 void YarnEngine::_notification(int p_what) {
@@ -87,16 +220,66 @@ void YarnEngine::set_is_paused(bool val) {
     }
 }
 
+
 void YarnEngine::handle_time_setting() {
     pause_independent_time = OS::get_singleton()->get_ticks_msec() * 0.001f;
     if (!is_paused) {
         time += pause_independent_time - amount_time_last_frame;
         amount_time_last_frame = pause_independent_time;
     }
+    if(save_next_frame) {
+        save_next_frame = false;
+        actually_save();
+    } else if (!save_next_frame && save_requested && (last_time_save_requested + 0.33 < pause_independent_time)) {
+        save_requested = false;
+        execute_save();
+    }
+}
 
-    // if save_requested and pause_independent_time_elapsed(last_time_save_requested,0.33):
-    //     save_requested = false
-    //     do_save()
+void YarnEngine::execute_save() {
+    emit_signal(SNAME("prepare_save"));
+    save_next_frame = true;
+}
+
+void YarnEngine::actually_save() {
+    auto file = FileAccess::open(save_path,FileAccess::WRITE);
+    file->store_string(JSON::stringify(save_data));
+    file->close();
+    print_line("Saved");
+}
+
+bool YarnEngine::request_load() {
+    auto dir = DirAccess::create(DirAccess::ACCESS_USERDATA);
+    if (!dir->file_exists(save_path)) {
+        return false;
+    }
+    auto file = FileAccess::open(save_path, FileAccess::READ);
+    auto file_string = file->get_as_text();
+    if (file_string.is_empty())
+        return false;
+    Ref<JSON> jason;
+    jason.instantiate();
+    Error error = jason->parse(file_string);
+    if(error != Error::OK) {
+        jason.unref();
+        print_line(vformat("Parse JSON failed. Error at line %d: %s", jason->get_error_line(), jason->get_error_message()));
+        return false;
+    }
+    save_data = jason->get_data();
+    Variant args[1] = { save_data };
+    const Variant *argptrs[1] = { &args[0] };
+    emit_signalp(SNAME("loaded_save"),argptrs,1);
+    return true;
+}
+
+void YarnEngine::request_save(bool immediate = false) {
+    if(immediate) {
+        emit_signal(SNAME("prepare_save"));
+        actually_save();
+    } else {
+        save_requested = true;
+        last_time_save_requested = pause_independent_time;
+    }
 }
 
 bool YarnEngine::has_time_elapsed(float test_time, float interval) {
--- yarnengine.h
+++ yarnengine.h
@@ -11,6 +11,14 @@ class YarnEngine : public Node {
 protected:
     void setup_node();
 
+    void create_save_backup();
+
+    Variant get_from_save(String p_save_key, Variant p_save_default);
+
+    YarnEngine *set_to_save(String p_save_key, Variant p_save_value);
+
+    YarnEngine *add_to_save_data(String p_save_key, Variant p_save_value);
+
     static void _bind_methods();
 
     Node *get_current_scene();
@@ -29,18 +37,49 @@ protected:
 public:
     bool already_setup_in_tree = false;
 
+    String save_path = "user://game.save";
+    String get_save_path() {return save_path;}
+    void set_save_path(String val) {save_path = val;}
+
+    String save_backup_path = "user://game.save.backup";
+    String get_save_backup_path() {return save_backup_path;}
+    void set_save_backup_path(String val) {save_backup_path = val;}
+
+    String save_reset_backup_path = "user://game.reset.save.backup";
+    String get_save_reset_backup_path() {return save_reset_backup_path;}
+    void set_save_reset_backup_path(String val) {save_reset_backup_path = val;}
+
+    bool save_requested;
+    bool get_save_requested() {return save_requested;}
+    void set_save_requested(bool val) {save_requested = val;}
+
+    float last_time_save_requested;
+    bool save_next_frame;
+    Dictionary save_data;
+    Dictionary get_save_data() {return save_data;}
+    void set_save_data(const Dictionary &val) { save_data = val;};
+
+
     bool is_paused;
     bool get_is_paused() {return is_paused;}
     void set_is_paused(bool val);
 
     float time;
     float get_time() {return time;}
-    void set_time(float val) {time = val;}
+    void set_time(const float val) {time = val;}
 
     float pause_independent_time;
     float get_pause_independent_time() {return pause_independent_time;}
-    void set_pause_independent_time(float val) {pause_independent_time = val;}
+    void set_pause_independent_time(const float val) {pause_independent_time = val;}
     void handle_time_setting();
+
+    void execute_save();
+
+    void actually_save();
+
+    bool request_load();
+
+    void request_save(bool immediate);
 
     bool has_time_elapsed(float test_time, float interval);
 
