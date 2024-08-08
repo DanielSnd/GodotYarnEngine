e515bb651427c2a9a7a4649aa207d100a6f68bd2
Daniel Snd <daniel@bittentoast.com>
2024-08-08 13:08:08 -0300

    Added more dummy fmod resources to event asset

--- disabled_fmod/eventasset.cpp
+++ disabled_fmod/eventasset.cpp
@@ -103,19 +103,135 @@ bool EventAsset::has_parameter(const String& parameter_guid) const
 	return parameters.has(parameter_guid);
 }
 
-void EventAsset::set_guid(const String& p_guid) {
+void FMODAsset::_bind_methods()
+{
+	ClassDB::bind_method(D_METHOD("set_guid", "guid"), &FMODAsset::set_guid);
+	ClassDB::bind_method(D_METHOD("get_guid"), &FMODAsset::get_guid);
+
+	ClassDB::bind_method(D_METHOD("set_fmod_path", "path"), &FMODAsset::set_fmod_path);
+	ClassDB::bind_method(D_METHOD("get_fmod_path"), &FMODAsset::get_fmod_path);
+
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "fmod_path", PropertyHint::PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_fmod_path", "get_fmod_path");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "guid", PropertyHint::PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+
+			"set_guid", "get_guid");
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "name", PropertyHint::PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_name", "get_name");
+}
+
+void FMODAsset::set_guid(const String& p_guid) {
 	guid = p_guid;
 }
 
-String EventAsset::get_guid() const {
+String FMODAsset::get_guid() const {
 	return guid;
 }
-String EventAsset::get_fmod_path() {
+String FMODAsset::get_fmod_path() {
 	return fmod_path;
 }
 
-void EventAsset::set_fmod_path(const String &_path) {
+void FMODAsset::set_fmod_path(const String &_path) {
 	fmod_path = _path;
+}
+
+void FMODGuidDictionary::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_guid_dictionary", "guid_dictionary"), &FMODGuidDictionary::set_guid_dictionary);
+	ClassDB::bind_method(D_METHOD("get_guid_dictionary"), &FMODGuidDictionary::get_guid_dictionary);
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "guid_dictionary", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_guid_dictionary", "get_guid_dictionary");
+}
+
+Dictionary FMODGuidDictionary::get_guid_dictionary() const {
+	return guid_dictionary;
+}
+
+void FMODGuidDictionary::set_guid_dictionary(const Dictionary &p_guid_dictionary) {
+	guid_dictionary = p_guid_dictionary;
+}
+
+void BusAsset::_bind_methods() {
+}
+
+void BusAsset::set_bus_ref(const Dictionary bus) {
+}
+
+void VCAAsset::_bind_methods() {
+}
+
+void VCAAsset::set_vca_ref(const Dictionary vca) {
+}
+
+void ParameterAsset::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_parameter_description", "parameter_description"),
+		&ParameterAsset::set_parameter_description);
+	ClassDB::bind_method(D_METHOD("get_parameter_description"), &ParameterAsset::get_parameter_description);
+	ClassDB::bind_method(D_METHOD("set_labels", "labels"), &ParameterAsset::set_labels);
+	ClassDB::bind_method(D_METHOD("get_labels"), &ParameterAsset::get_labels);
+
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "parameter_description", PropertyHint::PROPERTY_HINT_RESOURCE_TYPE,
+						 "FMOD_STUDIO_PARAMETER_DESCRIPTION", PROPERTY_USAGE_DEFAULT),
+			"set_parameter_description", "get_parameter_description");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "labels"), "set_labels", "get_labels");
+}
+
+void ParameterAsset::set_parameter_ref(const Dictionary parameter_description) {
+
+}
+
+void ParameterAsset::set_parameter_description(const Dictionary parameter_description) {
+
+}
+
+Dictionary ParameterAsset::get_parameter_description() const {
+	return Dictionary{};
+}
+
+void ParameterAsset::set_labels(const Array &p_labels) {
+	labels = p_labels;
+}
+
+Array ParameterAsset::get_labels() const {
+	return labels;
+}
+
+void BankAsset::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_file_path", "file_path"), &BankAsset::set_file_path);
+	ClassDB::bind_method(D_METHOD("get_file_path"), &BankAsset::get_file_path);
+	ClassDB::bind_method(D_METHOD("set_modified_time", "modified_time"), &BankAsset::set_modified_time);
+	ClassDB::bind_method(D_METHOD("get_modified_time"), &BankAsset::get_modified_time);
+
+	ADD_PROPERTY(
+			PropertyInfo(Variant::STRING, "file_path", PropertyHint::PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_file_path", "get_file_path");
+
+	ADD_PROPERTY(
+			PropertyInfo(Variant::STRING, "modified_time", PropertyHint::PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_modified_time", "get_modified_time");
+}
+
+void BankAsset::set_bank_file_info(const Dictionary &file_bank_info) {
+
+}
+
+void BankAsset::set_bank_ref(const Dictionary bank) {
+
+}
+
+void BankAsset::set_file_path(const String &p_file_path) {
+	file_path = p_file_path;
+}
+
+String BankAsset::get_file_path() const {
+	return file_path;
+}
+
+void BankAsset::set_modified_time(int p_modified_time) {
+	modified_time = p_modified_time;
+}
+
+int BankAsset::get_modified_time() const {
+	return modified_time;
 }
 
 void EventAsset::_bind_methods()
@@ -162,4 +278,122 @@ void EventAsset::_bind_methods()
 			"set_parameters", "get_parameters");
 }
 
+void ProjectCache::_bind_methods()
+{
+	ClassDB::bind_method(D_METHOD("set_banks", "banks"), &ProjectCache::set_banks);
+	ClassDB::bind_method(D_METHOD("get_banks"), &ProjectCache::get_banks);
+	ClassDB::bind_method(D_METHOD("set_events", "events"), &ProjectCache::set_events);
+	ClassDB::bind_method(D_METHOD("get_events"), &ProjectCache::get_events);
+	ClassDB::bind_method(D_METHOD("set_snapshots", "snapshots"), &ProjectCache::set_snapshots);
+	ClassDB::bind_method(D_METHOD("get_snapshots"), &ProjectCache::get_snapshots);
+	ClassDB::bind_method(D_METHOD("set_busses", "busses"), &ProjectCache::set_busses);
+	ClassDB::bind_method(D_METHOD("get_busses"), &ProjectCache::get_busses);
+	ClassDB::bind_method(D_METHOD("set_vcas", "vcas"), &ProjectCache::set_vcas);
+	ClassDB::bind_method(D_METHOD("get_vcas"), &ProjectCache::get_vcas);
+	ClassDB::bind_method(D_METHOD("set_parameters", "parameters"), &ProjectCache::set_parameters);
+	ClassDB::bind_method(D_METHOD("get_parameters"), &ProjectCache::get_parameters);
+	ClassDB::bind_method(D_METHOD("set_guid_to_path", "guid_to_path"), &ProjectCache::set_guid_to_path);
+	ClassDB::bind_method(D_METHOD("get_guid_to_path"), &ProjectCache::get_guid_to_path);
+	ClassDB::bind_method(D_METHOD("set_bank_tree", "bank_tree"), &ProjectCache::set_bank_tree);
+	ClassDB::bind_method(D_METHOD("get_bank_tree"), &ProjectCache::get_bank_tree);
+	ClassDB::bind_method(D_METHOD("set_event_tree", "event_tree"), &ProjectCache::set_event_tree);
+	ClassDB::bind_method(D_METHOD("get_event_tree"), &ProjectCache::get_event_tree);
+	ClassDB::bind_method(D_METHOD("set_snapshot_tree", "snapshot_tree"), &ProjectCache::set_snapshot_tree);
+	ClassDB::bind_method(D_METHOD("get_snapshot_tree"), &ProjectCache::get_snapshot_tree);
+	ClassDB::bind_method(D_METHOD("set_bus_tree", "bus_tree"), &ProjectCache::set_bus_tree);
+	ClassDB::bind_method(D_METHOD("get_bus_tree"), &ProjectCache::get_bus_tree);
+	ClassDB::bind_method(D_METHOD("set_vca_tree", "vca_tree"), &ProjectCache::set_vca_tree);
+	ClassDB::bind_method(D_METHOD("get_vca_tree"), &ProjectCache::get_vca_tree);
+	ClassDB::bind_method(D_METHOD("set_parameter_tree", "parameter_tree"), &ProjectCache::set_parameter_tree);
+	ClassDB::bind_method(D_METHOD("get_parameter_tree"), &ProjectCache::get_parameter_tree);
+
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "banks", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_banks", "get_banks");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "events", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_events", "get_events");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "snapshots", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_snapshots", "get_snapshots");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "vcas", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_vcas",
+			"get_vcas");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "busses", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_busses", "get_busses");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "parameters", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+	"set_parameters", "get_parameters");
+	ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "guid_to_path", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT),
+			"set_guid_to_path", "get_guid_to_path");
+
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "bank_tree", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+			"set_bank_tree", "get_bank_tree");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "event_tree", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+			"set_event_tree", "get_event_tree");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "snapshot_tree", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+			"set_snapshot_tree", "get_snapshot_tree");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "vca_tree", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+			"set_vca_tree", "get_vca_tree");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "bus_tree", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+			"set_bus_tree", "get_bus_tree");
+	ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "parameter_tree", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_STORAGE),
+			"set_parameter_tree", "get_parameter_tree");
+}
+
+void ProjectCache::initialize_cache(const Dictionary& project_info) {
+
+	set_banks(project_info["banks"]);
+	set_events(project_info["events"]);
+	set_snapshots(project_info["snapshots"]);
+	set_vcas(project_info["vcas"]);
+	set_busses(project_info["busses"]);
+	set_parameters(project_info["parameters"]);
+	set_guid_to_path(project_info["guid_to_path"]);
+
+	const String cache_dir = "res://FMOD/cache";
+	Ref<DirAccess> dir = DirAccess::open(cache_dir);
+	if (dir.is_null())
+	{
+		DirAccess::make_dir_absolute(cache_dir);
+		dir = DirAccess::open(cache_dir);
+	}
+
+	const String cache_path = "res://FMOD/cache/fmod_project_cache.tres";
+
+	set_path(cache_path, true);
+	ResourceSaver::save(this, cache_path);
+	print_line(vformat("[FMOD] Cache created in %s",cache_path));
+	emit_changed();
+
+	const String resources_path = "res://FMOD/editor/resources";
+	Dictionary resource_dirs;
+	resource_dirs["events"] = "res://FMOD/editor/resources/events/";
+	resource_dirs["snapshots"] = "res://FMOD/editor/resources/snapshots/";
+	resource_dirs["busses"] = "res://FMOD/editor/resources/busses/";
+	resource_dirs["vcas"] = "res://FMOD/editor/resources/vcas/";
+	resource_dirs["parameters"] = "res://FMOD/editor/resources/parameters/";
+	Array all_fmod_paths;
+	all_fmod_paths.append_array(get_events().keys());
+	all_fmod_paths.append_array(get_snapshots().keys());
+	all_fmod_paths.append_array(get_busses().keys());
+	all_fmod_paths.append_array(get_vcas().keys());
+	all_fmod_paths.append_array(get_parameters().keys());
+	// print_line(vformat("All fmod paths %s",all_fmod_paths));
+	for (const auto& dir_path: resource_dirs.values()) {
+		Ref<DirAccess> new_dir = DirAccess::open(dir_path);
+		if (!new_dir.is_null()) {
+			new_dir->list_dir_begin();
+			String file_name = new_dir->get_next();
+			while (!file_name.is_empty()) {
+				//print_line(vformat("filename [%s]. Load path [%s]",file_name,vformat("%s/%s",dir_path,file_name)));
+				if (file_name.ends_with(".tres") && (!all_fmod_paths.has(file_name.replace(".tres","")))) {
+					Ref<FMODAsset> loaded_asset = ResourceLoader::load(vformat("%s/%s",dir_path,file_name));
+					if (loaded_asset.is_valid()) {
+						print_line(vformat("[FMOD] FMOD Asset [%s] path [%s] not found in fmod bank",file_name,loaded_asset->get_fmod_path()));
+					}
+					else {
+						print_line(vformat("[FMOD] FMOD Asset [%s] not found in fmod bank",file_name));
+					}
+				}
+				file_name = new_dir->get_next();
+			}
+		}
+	}
+}
 #endif
--- disabled_fmod/eventasset.h
+++ disabled_fmod/eventasset.h
@@ -8,9 +8,106 @@
 #include "core/io/resource.h"
 #include "scene/main/node.h"
 #include "core/object/ref_counted.h"
+#include "core/io/dir_access.h"
 
-class EventAsset : public Resource {
-    GDCLASS(EventAsset, Resource);
+class FMODAsset : public Resource
+{
+    GDCLASS(FMODAsset, Resource);
+
+protected:
+    static void _bind_methods();
+
+public:
+    void set_guid(const String& guid);
+    String get_guid() const;
+    String get_fmod_path();
+
+    void set_fmod_path(const String &_path);
+
+private:
+    String fmod_path{};
+    String guid{};
+};
+
+class FMODGuidDictionary : public Resource
+{
+    GDCLASS(FMODGuidDictionary, Resource);
+
+protected:
+    static void _bind_methods();
+
+public:
+    Dictionary get_guid_dictionary() const;
+    void set_guid_dictionary(const Dictionary &p_guid_dictionary);
+    Dictionary guid_dictionary;
+
+};
+
+
+class BusAsset : public FMODAsset
+{
+    GDCLASS(BusAsset, FMODAsset);
+
+protected:
+    static void _bind_methods();
+
+public:
+    void set_bus_ref( Dictionary bus);
+};
+
+class VCAAsset : public FMODAsset
+{
+    GDCLASS(VCAAsset, FMODAsset);
+
+protected:
+    static void _bind_methods();
+
+public:
+    void set_vca_ref( Dictionary vca);
+};
+
+class ParameterAsset : public FMODAsset
+{
+    GDCLASS(ParameterAsset, FMODAsset);
+
+protected:
+    static void _bind_methods();
+
+private:
+    Array labels;
+
+public:
+    void set_parameter_ref(const Dictionary parameter_description);
+
+    void set_parameter_description(const Dictionary parameter_description);
+    Dictionary get_parameter_description() const;
+
+    void set_labels(const Array& labels);
+    Array get_labels() const;
+};
+
+class BankAsset : public FMODAsset
+{
+    GDCLASS(BankAsset, FMODAsset);
+
+protected:
+    static void _bind_methods();
+
+public:
+    void set_bank_file_info(const Dictionary& file_bank_info);
+    void set_bank_ref(Dictionary bank);
+    void set_file_path(const String& file_path);
+    String get_file_path() const;
+    void set_modified_time(int modified_time);
+    int get_modified_time() const;
+
+private:
+    String file_path{};
+    int modified_time{};
+};
+
+class EventAsset : public FMODAsset {
+    GDCLASS(EventAsset, FMODAsset);
 
 protected:
     static void _bind_methods();
@@ -51,16 +148,7 @@ public:
 
     bool has_parameter(const String& parameter_guid) const;
 
-public:
-    void set_guid(const String& p_guid);
-    String get_guid() const;
-    String get_fmod_path();
-
-    void set_fmod_path(const String &_path);
-
 private:
-    String fmod_path{};
-    String guid{};
     bool is_3d{};
     bool is_oneshot{};
     bool is_snapshot{};
@@ -69,6 +157,112 @@ private:
     Dictionary parameters;
 };
 
+
+class ProjectCache : public Resource
+{
+    GDCLASS(ProjectCache, Resource);
+
+protected:
+    static void _bind_methods();
+
+private:
+    Array bank_tree;
+    Array event_tree;
+    Array snapshot_tree;
+    Array bus_tree;
+    Array vca_tree;
+    Array parameter_tree;
+
+public:
+    Dictionary banks;
+    Dictionary events;
+    Dictionary snapshots;
+    Dictionary busses;
+    Dictionary vcas;
+    Dictionary parameters;
+    Dictionary guid_to_path;
+
+    void initialize_cache(const Dictionary& p_project_info);
+
+    void set_banks(const Dictionary& p_banks) {
+        banks = p_banks;
+    }
+    Dictionary get_banks() const
+    { return banks; }
+    void set_events(const Dictionary& p_events) {
+        events = p_events;
+    }
+    Dictionary get_events() const {
+        return events;
+    }
+    void set_snapshots(const Dictionary& p_snapshots) {
+        snapshots = p_snapshots;
+    }
+    Dictionary get_snapshots() const {
+        return snapshots;
+    }
+    void set_busses(const Dictionary& p_busses) {
+        busses = p_busses;
+    }
+    Dictionary get_busses() const {
+        return busses;
+    }
+    void set_vcas(const Dictionary& p_vcas) {
+        vcas = p_vcas;
+    }
+    Dictionary get_vcas() const {
+        return vcas;
+    }
+    void set_parameters(const Dictionary& p_parameters) {
+        parameters = p_parameters;
+    }
+    Dictionary get_parameters() const {
+        return parameters;
+    }
+    void set_guid_to_path(const Dictionary& p_guid_to_path) {
+        guid_to_path = p_guid_to_path;
+    }
+    Dictionary get_guid_to_path() const {
+        return guid_to_path;
+    }
+    void delete_invalid_event_assets() {
+
+    }
+    void set_bank_tree(const Array& p_bank_tree) {
+        bank_tree = p_bank_tree;
+    }
+    Array get_bank_tree() {
+        return bank_tree;
+    }
+    void set_event_tree(const Array& p_event_tree) {
+        event_tree = p_event_tree;
+    }
+    Array get_event_tree();
+    void set_snapshot_tree(const Array& p_snapshot_tree) {
+        snapshot_tree = p_snapshot_tree;
+    }
+    Array get_snapshot_tree() {
+        return snapshot_tree;
+    }
+    void set_bus_tree(const Array& p_bus_tree) {
+        bus_tree = p_bus_tree;
+    }
+    Array get_bus_tree() {
+        return bus_tree;
+    }
+    void set_vca_tree(const Array& p_vca_tree) {
+        vca_tree = p_vca_tree;
+    }
+    Array get_vca_tree() {
+        return vca_tree;
+    }
+    void set_parameter_tree(const Array& p_parameter_tree) {
+        parameter_tree = p_parameter_tree;
+    }
+    Array get_parameter_tree() {
+        return parameter_tree;
+    }
+};
 #endif
 
 #endif //EVENTASSET_H
